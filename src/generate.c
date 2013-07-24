7cef52a51c640711fee92d2a5019d1b4f34c6969
super <super@su10.(none)>
2013-07-24 11:25:04 +0800

    1. add two sample input file : device.in volume.in 2. sorm_geneate can accept multiple intput files

--- src/generate.c
+++ src/generate.c
@@ -28,44 +28,48 @@ extern FILE *yyin;
 int main(int argc, char **argv)
 {
     sorm_list_t *pos, *scratch;
-    int i;
+    int i, argv_index;
 
-    if(argc != 2)
+    if(argc < 2)
     {
-        printf("Usage : sorm_generate input_file\n");
-        return -1;
+        printf("Usage : sorm_generate input_files, input files are separated by space\n");
+	return -1;
     }
 
-    table_desc = mem_malloc(sizeof(sorm_table_descriptor_t));
-    memset(table_desc, 0, sizeof(sorm_table_descriptor_t));
-    
-    columns_list_head = mem_malloc(sizeof(sorm_list_t));
-    INIT_LIST_HEAD(columns_list_head);
-
-    column_desc = mem_malloc(sizeof(sorm_column_descriptor_t));
-    memset(column_desc, 0, sizeof(sorm_column_descriptor_t));
-    
-    yyin = fopen(argv[1], "r");
-
-    yyparse();
-
-    mem_free(column_desc);
-    column_desc = NULL;
-
-    table_desc->columns = malloc(sizeof(sorm_column_descriptor_t) * 
-            table_desc->columns_num);
-    i = 0;
-    sorm_list_for_each_safe(pos, scratch, columns_list_head)
+    for(argv_index = 1; argv_index < argc; argv_index ++)
     {
-        memcpy(&table_desc->columns[i], pos->data, 
-                sizeof(sorm_column_descriptor_t));
-        mem_free(pos);
-        i ++;
-    }
-    mem_free(columns_list_head);
+	table_desc = mem_malloc(sizeof(sorm_table_descriptor_t));
+	memset(table_desc, 0, sizeof(sorm_table_descriptor_t));
 
-    header_generate(table_desc);
-    c_generate(table_desc);
+	columns_list_head = mem_malloc(sizeof(sorm_list_t));
+	INIT_LIST_HEAD(columns_list_head);
+
+	column_desc = mem_malloc(sizeof(sorm_column_descriptor_t));
+	memset(column_desc, 0, sizeof(sorm_column_descriptor_t));
+
+	yyin = fopen(argv[argv_index], "r");
+
+	yyparse();
+	
+	mem_free(column_desc);
+	column_desc = NULL;
+
+	table_desc->columns = malloc(sizeof(sorm_column_descriptor_t) * 
+		table_desc->columns_num);
+	i = 0;
+	sorm_list_for_each_safe(pos, scratch, columns_list_head)
+	{
+	    memcpy(&table_desc->columns[i], pos->data, 
+		    sizeof(sorm_column_descriptor_t));
+	    mem_free(pos);
+	    i ++;
+	}
+	mem_free(columns_list_head);
+
+	header_generate(table_desc);
+	c_generate(table_desc);
+	mem_free(table_desc);
+    }
     
     return 0;
 }
