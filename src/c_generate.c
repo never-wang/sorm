fbfef436d93c752d1903f3df37f818fecefeb4fd
super <super@su10.(none)>
2013-07-24 16:54:32 +0800

    fix some bug

--- src/c_generate.c
+++ src/c_generate.c
@@ -16,33 +16,11 @@
 #include <errno.h>
 
 #include "c_generate.h"
+#include "generate.h"
 #include "sorm.h"
 #include "memory.h"
 
-
-#define INDENT "    "
-#define INDENT_TWICE INDENT INDENT
-#define INDENT_TRIPLE INDENT_TWICE INDENT
-#define HEADER_NAME_MAX_LEN 127
-
 #define _str(string) ((string) == NULL ? "NULL" : (string))
-
-static inline void case_lower2upper(char *lower_case, char *upper_case)
-{
-    int i;
-
-    while(lower_case[i] != '\0')
-    {
-        if(lower_case[i] <= 'z' && lower_case[i] >= 'a')
-        {
-            upper_case[i] = lower_case[i] - 32;
-        }else{
-            upper_case[i] = lower_case[i];
-        }
-        i++;
-    }
-    upper_case[i] = '\0';
-}
 
 static void c_generate_column_desc(
         FILE *file, const sorm_table_descriptor_t* table_desc)
--- src/generate.h
+++ src/generate.h
@@ -21,4 +21,26 @@ extern sorm_table_descriptor_t *table_desc;
 extern sorm_list_t *columns_list_head;
 extern sorm_column_descriptor_t *column_desc;
 
+#define INDENT "    "
+#define INDENT_TWICE INDENT INDENT
+#define INDENT_TRIPLE INDENT_TWICE INDENT
+#define HEADER_NAME_MAX_LEN 127
+
+static inline void case_lower2upper(char *lower_case, char *upper_case)
+{
+    int i = 0;
+
+    while(lower_case[i] != '\0')
+    {
+        if(lower_case[i] <= 'z' && lower_case[i] >= 'a')
+        {
+            upper_case[i] = lower_case[i] - 32;
+        }else{
+            upper_case[i] = lower_case[i];
+        }
+        i++;
+    }
+    upper_case[i] = '\0';
+}
+
 #endif
